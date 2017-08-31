6617b28ec343550be16135b6b906c2374705af61
Sterling <sterlingmd17@gmail.com>
2017-08-31 16:59:06 -0700

    finished exercises

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,39 +1,75 @@
 package org.launchcode.cheesemvc.controllers;
 
-
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
+/**
+ * Created by LaunchCode
+ */
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String,String> cheeses = new HashMap<>();
 
-    @RequestMapping(value="")
-    public String index(Model model){
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
 
-
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
+
         return "cheese/index";
     }
 
-    @RequestMapping(value="add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model){
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
+
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName,
-                                       @RequestParam String cheeseDescription){
-        cheeses.put(cheeseName, cheeseDescription);
-        //redirect to /cheese
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+        CheeseData.add(newCheese);
         return "redirect:";
     }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
+        }
+
+        return "redirect:";
+    }
+
+    @RequestMapping ( value = "edit/{cheeseId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int cheeseId){
+
+      model.addAttribute("cheese", CheeseData.getById(cheeseId));
+      return "cheese/edit";
+
+    }
+
+    @RequestMapping(value = "edit", method = RequestMethod.POST)
+    public String processEditForm(int cheeseId, String name, String description){
+
+        Cheese theCheese = CheeseData.getById(cheeseId);
+        theCheese.setName(name);
+        theCheese.setDescription(description);
+        return "redirect:";
+    }
+
 }
---
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -0,0 +1,53 @@
+package org.launchcode.cheesemvc.models;
+
+public class Cheese {
+
+    private String name;
+    private String description;
+    private int cheeseId;
+    private static int nextId =1;
+
+    public Cheese(String name, String description) {
+        this();
+        this.name = name;
+        this.description = description;
+    }
+
+    public Cheese(){
+        cheeseId=nextId;
+        nextId++;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Cheese.nextId = nextId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
